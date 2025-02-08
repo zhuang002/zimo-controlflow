9c4554a8a503e3ab6b080dacdb78be5f7a267674
zhuang002 <zhuang001@gmail.com>
2025-02-07 21:04:04 -0500

    implement while clause

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
 
 public class Main {
 
+	/* spec 1
+	 * Input numbers and sum them up until the number is negative.
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int sum = 0;
+		int a = 0;
+		
+		/*
+		 * while (condition) {
+		 * 		statements;
+		 * }
+		 * 
+		 * condition: is a boolean expression or value. 
+		 * if it is true then enter the loop, otherwise break out.
+		 */
+		
+		//spec 1
+		while (a>=0) {
+			sum += a;
+			a = sc.nextInt();
+		}
+		
+		System.out.println(sum);
 	}
 
 }
