51d56fb69989f5034c2641afc9c8a881da0b41a0
zhuang002 <zhuang001@gmail.com>
2025-02-12 20:15:29 -0500

    first implementation of accumlation

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int loops = sc.nextInt();
+		for (int i=0;i<loops;i++) {
+			int n = sc.nextInt();
+			int sum = 0;
+			for (int j=1;j<=n;j++) {
+				sum+=j;
+			}
+			System.out.println(sum);
+		}
 	}
 
 }
