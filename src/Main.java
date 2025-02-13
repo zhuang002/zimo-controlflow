614570d10401f56a47066f8b3da7bf5360ed79f2
zhuang002 <zhuang001@gmail.com>
2025-02-12 20:37:43 -0500

    implement an area() method

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,27 @@ public class Main {
 		for (int i=0;i<loops;i++) {
 			System.out.println(accumulate(sc.nextInt()));
 		}
+		
+		System.out.println("Please input 3 integers indicating the edge length of a triangle");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		if (a+b<=c || b+c<=a || c+a<=b) {
+			System.out.println("Error edges.");
+			return;
+		}
+		
+		System.out.println("The area of the triangle is "+area(a,b,c));
+	}
+
+	private static double area(double a, double b, double c) {
+		// TODO Auto-generated method stub
+		// S = sqrt(p*(p-a)*(p-b)*(p-c)), p=(a+b+c)/2;
+		
+		double p=(a+b+c)/2;
+		return Math.sqrt(p*(p-a)*(p-b)*(p-c));
+		
 	}
 
 	private static int accumulate(int n) {
